b226110f0f6932841b0ead43c07f70cdf18bad1b
vhorielova <horielova.viktoria@gmail.com>
2025-11-02 17:40:26 +0200

    (back-end): initialize back-end on spring

---
+++ back-end/film-library-service/src/main/java/com/fcsc/pi/framehouse/FramehouseApplication.java
@@ -0,0 +1,13 @@
+package com.fcsc.pi.framehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramehouseApplication.class, args);
+	}
+
+}
