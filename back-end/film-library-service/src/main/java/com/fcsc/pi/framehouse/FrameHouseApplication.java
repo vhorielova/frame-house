1d529971dfe640e8eda23deffc78a9fb411d745b
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-04 22:33:31 +0200

    (back/s3/feature): renamed FrameHouseApplication

---
+++ back-end/film-library-service/src/main/java/com/fcsc/pi/framehouse/FrameHouseApplication.java
@@ -0,0 +1,13 @@
+package com.fcsc.pi.framehouse;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameHouseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FrameHouseApplication.class, args);
+	}
+
+}
