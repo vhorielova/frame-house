1bfe8ea46e47c66c7e42779c0a14a5d064737e7d
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-04 22:30:33 +0200

    (back/s3/feature): added and unittesed S3StorageService

--- back-end/film-library-service/src/main/java/com/fcsc/pi/framehouse/service/IStorageService.java
+++ back-end/film-library-service/src/main/java/com/fcsc/pi/framehouse/service/IStorageService.java
@@ -1,7 +1,8 @@
 package com.fcsc.pi.framehouse.service;
 
 import com.fcsc.pi.framehouse.exceptions.storageservice.FileAlreadyExistsException;
-import com.fcsc.pi.framehouse.exceptions.storageservice.FileDoesNotExist;
+import com.fcsc.pi.framehouse.exceptions.storageservice.FileDoesNotExistException;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -10,9 +11,9 @@ public interface IStorageService {
 
     void save(MultipartFile file) throws IOException, FileAlreadyExistsException;
 
-    void delete(String filename) throws FileDoesNotExist;
+    void delete(String filename) throws FileDoesNotExistException;
 
     boolean doesFileExist(String filename);
 
-    MultipartFile load(String filename);
+    Resource load(String filename) throws IOException;
 }
