24bbd07254eda2781a3df56ce5eaadb805be236f
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-04 22:29:54 +0200

    (back/s3/refactor): renamed FileDoesNotExist exception

--- back-end/film-library-service/src/main/java/com/fcsc/pi/framehouse/exceptions/storageservice/FileDoesNotExist.java
+++
@@ -1,7 +0,0 @@
-package com.fcsc.pi.framehouse.exceptions.storageservice;
-
-public class FileDoesNotExist extends RuntimeException {
-    public FileDoesNotExist(String message) {
-        super(message);
-    }
-}
