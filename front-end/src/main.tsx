05c76351e7713edee9271de6d5a8de3fd51523e2
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2026-01-03 22:32:56 +0200

    (front/router): add routing

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import AddBook from './pages/add-book/AddBook.tsx'
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App/>,
+    children: [
+      { path: "/", element: <h1>Wellcome to Frame House</h1> },
+      { path: "/add-film", element: <AddBook/> },
+      { path: "/profile", element: <h1>Profile page is not ready yet</h1> },
+      { path: "*", element: <h1>Woops! 404: page is not found</h1> }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
--- front-end/src/pages/add-book/AddBook.tsx
+++ front-end/src/pages/add-book/AddBook.tsx
@@ -1,17 +1,10 @@
 
-import Navbar from "../../common/Navbar"
 import "./AddBook.css"
 import AddBookForm from "./AddBookForm"
 
 export default function AddBook() {
 
-
-    return <div className="page">
-        <Navbar/>
-
-        <div className="content">
-            <AddBookForm/>    
-        </div>    
-        
+    return <div className="content">
+        <AddBookForm/>            
     </div>
 }
